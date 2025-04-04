18a8a66e8a04fb5ece7b83e98ee1d2e703e47e87
NovaLogics <rajitha.shavinda@gmail.com>
2025-04-04 16:19:07 +0530

    feat(auth): auth functions

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Velvaine - Admin Auth",
+  title: "Velvaine - Admin Dashboard",
   description: "Dashboard for Velvaine",
 };
 

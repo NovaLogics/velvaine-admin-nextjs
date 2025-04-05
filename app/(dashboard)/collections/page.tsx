d7383bdfbc9b373ff1964cc93a297e2584bcf03e
NovaLogics <rajitha.shavinda@gmail.com>
2025-04-05 22:02:33 +0530

    feat(collection): navigation functions

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -4,9 +4,11 @@ import { DataTable } from "@/components/custom_ui/DataTable";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { PlusSquare } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const Collections = () => {
+  const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [collections, setCollections] = useState([]);
 
@@ -44,14 +46,17 @@ const Collections = () => {
       <div className="flex items-center justify-between">
         <p className="text-heading2-bold">Collections</p>
 
-        <Button className="bg-blue-500 text-white hover:bg-blue-600">
+        <Button
+          className="bg-blue-500 text-white hover:bg-blue-600"
+          onClick={() => router.push("/collections/create")}
+        >
           <PlusSquare className="h-8 w-8 mr-2" />
           Create Collection
         </Button>
       </div>
       <Separator className="bg-grey-1 my-5" />
 
-      <DataTable columns={columns} data={collections} searchKey="title"/>
+      <DataTable columns={columns} data={collections} searchKey="title" />
     </div>
   );
 };

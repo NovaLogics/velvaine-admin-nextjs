6e39a6c60f8cef254818d2d7dc5bc2ecca0b452d
NovaLogics <rajitha.shavinda@gmail.com>
2025-04-20 01:02:00 +0530

    feat(search): impl search feature

---
+++ app/api/search/[query]/route.ts
@@ -0,0 +1,31 @@
+import Product from "@/lib/models/Product";
+import { connectToDB } from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (
+  request: NextRequest,
+  { params }: { params: { query: string } }
+) => {
+  try {
+    await connectToDB();
+    const { query } = params;
+    const searchedProducts = await Product.find({
+      $or: [
+        {
+          title: { $regex: query, $options: "i" },
+        },
+        {
+          category: { $regex: query, $options: "i" },
+        },
+        {
+          tags: { $in: [new RegExp(query, "i")] },
+        },
+      ],
+    });
+
+    return NextResponse.json(searchedProducts, { status: 200 });
+  } catch (error) {
+    console.log("Error in GET /api/search", error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+};
